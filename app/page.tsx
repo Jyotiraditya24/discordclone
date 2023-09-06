008eb636ae96c2845d3880958c322261ed92019b
jyotiraditya <jyotiraditya24@gmail.com>
2023-09-06 09:28:15 +0530

    Environment setup Done

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <p className='bg-red-200'>Discord Clone <Button>Hello</Button> </p>
+    <p className='bg-red-200'>Discord Clone <Button variant="destructive">Hello</Button> </p>
   )
 }
