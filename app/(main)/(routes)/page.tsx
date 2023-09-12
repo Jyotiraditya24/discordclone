40eef9b1539a9bb4e33ddb99d3c64c99e26983f2
jyotiraditya <jyotiraditya24@gmail.com>
2023-09-12 11:33:57 +0530

    Authentication Done

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button"
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h3>This is a protected Route</h3>
+      <UserButton afterSignOutUrl="/" />
     </div>
-  )
+  );
 }
